93ea3b8805e7d84fc2ae17dc6d56f81b9be0bce8
romainaury <romain.aury@hotmail.com>
2022-12-15 12:40:39 +0100

    final commit

--- api/app.py
+++ api/app.py
@@ -80,11 +80,12 @@ def get_groupes():
 @app.route('/groupes', methods=['POST', ])
 def post_groupe():
     """"Crée un groupes"""
-    nom_groupe = request.args.get('nom_groupe')
+    nom_groupe = request.args.get('nom_groupe') or ""
     nom_groupe = urllib.parse.unquote(nom_groupe)
+    
     groupe = execute_query("select * from groupes where nom_groupe = ?", (nom_groupe, ))
     
-    if(nom_groupe is None or len(groupe) > 0):
+    if(len(nom_groupe) < 1 or len(groupe) > 0):
         abort(409)
         
     id = execute_query("insert into groupes(nom_groupe) values (?)", (nom_groupe,))
@@ -133,7 +134,7 @@ def get_groupe_concerts(nom_groupe):
     # ajout de _links aux concerts
     for i in range(0, len(concerts)):
         concerts[i]["_links"] = [{
-                "href": "/concerts/" + urllib.parse.quote(concerts[i]["id"]),
+                "href": "/concerts/" + str(concerts[i]["id"]),
                 "rel": "self"
             }]
         
@@ -148,13 +149,13 @@ def get_utilisateurs():
     utilisateurs = execute_query("select * from utilisateurs")
     if(len(utilisateurs) < 1):
         return jsonify({}), 204
-    # ajout de _links aux concerts
+    # ajout de _links aux utilisateurs
     for i in range(0, len(utilisateurs)):
         utilisateurs[i]["_links"] = [{
-                "href": "/utilisateurs/" + urllib.parse.quote(utilisateurs[i]["id"]),
+                "href": "/utilisateurs/" + str(utilisateurs[i]["id"]),
                 "rel": "self"
             }, {
-                "href": "/utilisateurs/" + urllib.parse.quote(utilisateurs[i]["id"]) + "/reservations",
+                "href": "/utilisateurs/" + str(utilisateurs[i]["id"]) + "/reservations",
                 "rel": "reservations"
             }]
         
@@ -167,12 +168,12 @@ def get_utilisateur(utilisateur_id):
     utilisateur = execute_query("select * from utilisateurs where id = ?", (utilisateur_id, ))
     if(len(utilisateur) < 1):
         abort(404)
-    # ajout de _links aux concerts
+    # ajout de _links aux utilisateurs
     utilisateur[0]["_links"] = [{
-            "href": "/utilisateurs/" + urllib.parse.quote(utilisateur[0]["id"]),
+            "href": "/utilisateurs/" + str(utilisateur[0]["id"]),
             "rel": "self"
         }, {
-            "href": "/utilisateurs/" + urllib.parse.quote(utilisateur[0]["id"]) + "/reservations",
+            "href": "/utilisateurs/" + str(utilisateur[0]["id"]) + "/reservations",
             "rel": "reservations"
         }]
         
@@ -181,10 +182,12 @@ def get_utilisateur(utilisateur_id):
 @app.route('/utilisateurs/', methods=['POST'])
 def post_utilisateur():
     """Crée un utilisateur"""
-    nom_utilisateur = request.args.get('nom_utilisateur')
+    nom_utilisateur = request.args.get('nom_utilisateur') or ""
+    nom_utilisateur = urllib.parse.unquote(nom_utilisateur)
+    
     utilisateur = execute_query("select * from utilisateurs where nom = ?", (nom_utilisateur, ))
     
-    if(nom_utilisateur is None or len(utilisateur) > 0):
+    if(len(nom_utilisateur) < 1 or len(utilisateur) > 0):
         abort(409)
         
     id = execute_query("insert into utilisateurs(nom_utilisateur) values (?)", (nom_utilisateur, ))
@@ -208,14 +211,19 @@ def delete_utilisateur(utilisateur_id):
 @app.route('/utilisateurs/<string:utilisateur_id>/reservations')
 def get_utilisateur_reservations(utilisateur_id):
     """Récupère les réservations d'un utilisateur"""
-    reservations = execute_query("select g.nom_groupe, c.date_concert, c.places_max from utilisateurs u, reservations r, concerts c, groupes g where u.id = ? and r.utilisateur_id = u.id and c.id = r.concert_id and g.id = c.groupe_id", (utilisateur_id, ))
+    reservations = execute_query("select r.id, g.nom_groupe, c.date_concert, c.places_max from utilisateurs u, reservations r, concerts c, groupes g where u.id = ? and r.utilisateur_id = u.id and c.id = r.concert_id and g.id = c.groupe_id", (utilisateur_id, ))
     
     if(len(reservations) < 1):
         return jsonify({}), 204
         
-    id = execute_query("delete from utilisateurs where id = ?", (utilisateur_id, ))
-
-    return jsonify({"id": id}), 200
+    # ajout de _links aux reservations
+    for i in range(0, len(reservations)):
+        reservations[i]["_links"] = [{
+                "href": "/reservation/" + urllib.parse.quote(reservations[i]["id"]),
+                "rel": "self"
+            }]
+        
+    return jsonify(reservations), 200
 
 
 """ ################# Concerts #################
@@ -223,17 +231,61 @@ def get_utilisateur_reservations(utilisateur_id):
     
     
 @app.route('/concerts')
-def get_utilisateur_reservations():
+def get_concerts():
     """Récupère les concerts"""
-    concerts = execute_query("select * from concerts")
+    concerts = execute_query("select g.nom_groupe, c.* from concerts c, groupes g where c.groupe_id = g.id")
     
     if(len(concerts) < 1):
         return jsonify({}), 204
         
+    # ajout de _links aux concerts
+    for i in range(0, len(concerts)):
+        concerts[i]["_links"] = [{
+                "href": "/concerts/" + urllib.parse.quote(concerts[i]["id"]),
+                "rel": "self"
+            }]
 
     return jsonify(concerts), 200
 
+    
+@app.route('/concerts', methods=['POST',])
+def post_concert():
+    """crée un concert"""
+    groupe_id = request.args.get('groupe_id') or ""
+    date_concert = request.args.get('date_concert') or ""
+    places_max = request.args.get('places_max') or ""
+    
+    groupe_id = urllib.parse.unquote(groupe_id)
+    date_concert = urllib.parse.unquote(date_concert)
+    places_max = urllib.parse.unquote(places_max)
+    
+    
+    concert = execute_query("select * from concerts")
+    
+    if(len(groupe_id) < 1 or len(date_concert) < 1 or len(places_max) < 1 or len(concert) > 0):
+        abort(409)
+    
+    id = execute_query("insert into concerts(groupe_id, date_concert, places_max) values (?, ?, ?)", (groupe_id, date_concert, places_max))
+    # ajout de _links aux concerts
+    concert[0]["_links"] = [{
+            "href": "/concerts/" + str(concert[0]["id"]),
+            "rel": "self"
+        }]
+    
+    return jsonify({"id": id})
 
+
+@app.route('/concerts/<string:concert_id>', methods=['DELETE'])
+def delete_concerts(concert_id):
+    """Récupère les concerts"""
+    concert = execute_query("select * from concerts where c.concert_id = ?", (concert_id,))
+    
+    if(len(concert) < 1):
+        return jsonify({}), 204
+    
+    execute_query("delete from concerts where concert_id = ?", (concert_id,))
+
+    return jsonify(concert), 200
 
 if __name__ == '__main__':
     # define the localhost ip and the port that is going to be used
