dd0c72d6b550e593798dbf4afff4445ab5b33509
romainaury <romain.aury@hotmail.com>
2022-12-15 11:31:17 +0100

    groupes

---
+++ api/app.py
@@ -0,0 +1,145 @@
+from flask import Flask, jsonify, abort, request
+import mariadb
+import urllib.parse
+
+app = Flask(__name__)
+
+app.config['JSON_AS_ASCII'] = False  # pour utiliser l'UTF-8 plutot que l'unicode
+
+
+def execute_query(query, data=()):
+    config = {
+        'host': 'mariadb',
+        'port': 3306,
+        'user': 'root',
+        'password': 'root',
+        'database': 'mydatabase'
+    }
+    """Execute une requete SQL avec les param associés"""
+    # connection for MariaDB
+    conn = mariadb.connect(**config)
+    # create a connection cursor
+    cur = conn.cursor()
+    # execute a SQL statement
+    cur.execute(query, data)
+
+    if cur.description:
+        # serialize results into JSON
+        row_headers = [x[0] for x in cur.description]
+        rv = cur.fetchall()
+        list_result = []
+        for result in rv:
+            list_result.append(dict(zip(row_headers, result)))
+        return list_result
+    else:
+        conn.commit()
+        return cur.lastrowid
+
+
+# we define the route /
+@app.route('/')
+def welcome():
+    liens = [{}]
+    liens[0]["_links"] = [{
+        "href": "/groupes",
+        "rel": "groupes"
+    }, {
+        "href": "/concerts",
+        "rel": "concerts"
+    }, {
+        "href": "/utilisateurs",
+        "rel": "utilisateurs"
+    }, {
+        "href": "/reservations",
+        "rel": "reservations"
+    }]
+    return jsonify(liens), 200
+
+
+""" ################# Groupes #################
+    #############################################"""
+
+@app.route('/groupes')
+def get_groupes():
+    """"Récupère les groupes"""
+    groupes = execute_query("select * from groupes")
+    if(len(groupes) < 1):
+        abort(204)
+        
+    # ajout de _links aux groupes
+    for i in range(0, len(groupes)):
+        groupes[i]["_links"] = [{
+                "href": "/groupes/" + urllib.parse.quote(groupes[i]["nom_groupe"]),
+                "rel": "self"
+            },{
+                "href": "/groupes/" + urllib.parse.quote(groupes[i]["nom_groupe"]) + "/concerts",
+                "rel": "concerts",
+            }]
+    return jsonify(groupes), 200
+
+@app.route('/groupes', methods=['POST', ])
+def post_groupe():
+    """"Crée un groupes"""
+    nom_groupe = request.args.get('nom_groupe')
+    groupe = execute_query("select * from groupes where nom_groupe = ?", (nom_groupe, ))
+    
+    if(len(nom_groupe) < 1 or len(groupe) > 0):
+        abort(409)
+        
+    id = execute_query("insert into groupes(nom_groupe) values (?)", (id,))
+    
+    return jsonify(id), 201
+
+
+@app.route('/groupes', methods=['DELETE', ])
+def delete_groupe():
+    """"Supprime un groupe"""
+    nom_groupe = request.args.get('nom_groupe')
+    groupe = execute_query("select * from groupes where nom_groupe = ?", (nom_groupe, ))
+    
+    if(len(nom_groupe) < 1 or len(groupe) < 1):
+        abort(204)
+        
+    execute_query("insert into groupes(nom_groupe) values (?)", (id,))
+    
+    return jsonify(groupe), 200
+
+@app.route('/groupes/<string:nom_groupe>')
+def get_groupe(nom_groupe):
+    """"Récupère les infos d'un groupe"""
+    groupe = execute_query("select * from groupes where nom_groupe = ?", (nom_groupe, ))
+    if(len(groupe) < 1):
+        abort(404)
+    # ajout de _links à la région
+    groupe[0]["_links"] = [{
+            "href": "/groupes/" + urllib.parse.quote(groupe[0]["nom_groupe"]),
+            "rel": "self"
+        },{
+            "href": "/groupes/" + urllib.parse.quote(groupe[0]["nom_groupe"]) + "/concerts",
+            "rel": "concerts",
+        }]
+    return jsonify(groupe), 200
+
+
+@app.route('/groupes/<string:nom_groupe>/concerts')
+def get_groupe_concerts(nom_groupe):
+    """"Récupère les concerts d'un groupe"""
+    concerts = execute_query("select c.* from groupes g, concerts c where nom_groupe = ? and c.groupe_id = g.id", (nom_groupe, ))
+    if(len(concerts) < 1):
+        abort(204)
+    # ajout de _links aux concerts
+    for i in range(0, len(concerts)):
+        concerts[i]["_links"] = [{
+                "href": "/concerts/" + urllib.parse.quote(concerts[i]["id"]),
+                "rel": "self"
+            }]
+        
+    return jsonify(concerts), 200
+
+""" ################# Utilisateurs #################
+    #############################################"""
+
+
+if __name__ == '__main__':
+    # define the localhost ip and the port that is going to be used
+    app.run(host='0.0.0.0', port=5000)
---
+++ sql_init.sql
@@ -0,0 +1,49 @@
+CREATE OR REPLACE TABLE groupes (
+    id int(11) AUTO_INCREMENT PRIMARY KEY,
+    nom_groupe VARCHAR(25) UNIQUE   
+) ;
+
+CREATE OR REPLACE TABLE utilisateurs (
+    id int(11) AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255)   
+) ;
+
+CREATE OR REPLACE TABLE concerts (
+    id int(11) AUTO_INCREMENT PRIMARY KEY,
+    groupe_id int(11),
+    date_concert VARCHAR(10),
+    places_max int(11),
+    FOREIGN KEY (groupe_id) REFERENCES groupes (id)
+) ;
+
+CREATE OR REPLACE TABLE reservations (
+    id int(11) AUTO_INCREMENT PRIMARY KEY,
+    concert_id int(11),
+    utilisateur_id int(11),
+    FOREIGN KEY (concert_id) REFERENCES concerts (id),
+    FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id) 
+) ;
+
+INSERT INTO groupes(id, nom_groupe) VALUES (1, "THE ZOZOS");
+INSERT INTO groupes(id, nom_groupe) VALUES (2, "THE BAMBOULIDA");
+INSERT INTO groupes(id, nom_groupe) VALUES (3, "THE ROCKEURS");
+
+INSERT INTO utilisateurs(id, nom) VALUES (1, "BENJAMIN");
+INSERT INTO utilisateurs(id, nom) VALUES (2, "ROMAIN");
+INSERT INTO utilisateurs(id, nom) VALUES (3, "CHRISTOPHER");
+INSERT INTO utilisateurs(id, nom) VALUES (4, "LOUNA");
+
+INSERT INTO concerts(id, groupe_id, date_concert, places_max) VALUES (1, 2, "15/12/2022", 200);
+INSERT INTO concerts(id, groupe_id, date_concert, places_max) VALUES (2, 1, "16/12/2022", 190);
+INSERT INTO concerts(id, groupe_id, date_concert, places_max) VALUES (3, 3, "17/12/2022", 300);
+INSERT INTO concerts(id, groupe_id, date_concert, places_max) VALUES (4, 3, "18/12/2022", 250);
+INSERT INTO concerts(id, groupe_id, date_concert, places_max) VALUES (5, 2, "19/12/2022", 200);
+
+INSERT INTO reservations(id, concert_id, utilisateur_id) VALUES (1, 3, 4);
+INSERT INTO reservations(id, concert_id, utilisateur_id) VALUES (2, 4, 2);
+INSERT INTO reservations(id, concert_id, utilisateur_id) VALUES (3, 1, 2);
+INSERT INTO reservations(id, concert_id, utilisateur_id) VALUES (4, 1, 1);
+INSERT INTO reservations(id, concert_id, utilisateur_id) VALUES (5, 1, 2);
+INSERT INTO reservations(id, concert_id, utilisateur_id) VALUES (6, 5, 1);
+INSERT INTO reservations(id, concert_id, utilisateur_id) VALUES (7, 5, 4);
+INSERT INTO reservations(id, concert_id, utilisateur_id) VALUES (8, 2, 4);
